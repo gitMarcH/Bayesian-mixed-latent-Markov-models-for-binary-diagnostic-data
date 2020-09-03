028f1a0e26ec5be3c164034ecc9dbc036468f4f8
Marc Henrion <henrion.marc@gmail.com>
2020-09-03 14:07:07 +0200

    Cleaning code.

--- lmm_Correlations.R
+++ lmm_Correlations.R
@@ -5,14 +5,13 @@ library(corrplot)
 library(rmcorr)
 
 rm(list=ls())
-setwd("~/../work/MLW_LSTM/")
 
 
 ##--------------------
 ## read data
 ##
 
-tt<-read.csv("GordonMelita_StatsSupportAngeziwe/data/TTRInvASensitivity20170724_corrected.csv")
+tt<-read.csv("data/TTRInvASensitivity20170724_corrected.csv")
 tt<-tt[!is.na(tt$Sample.ID) & !is.na(tt$Visit..),]
 
 
@@ -20,7 +19,7 @@ tt<-tt[!is.na(tt$Sample.ID) & !is.na(tt$Visit..),]
 ## read recruitment dates and compute actual timepoints (in months)
 ##
 
-dates<-read.csv("GordonMelita_StatsSupportAngeziwe/data/salexpoLIMSDataSetComplete.csv")
+dates<-read.csv("data/salexpoLIMSDataSetComplete.csv")
 dates$monthYear<-gsub("^[0-9]+/","",dates$DATE) # dates run from 08/2013 to 12/2014
 datesDictionary<-data.frame(monthYear=c(paste(sep="/",c("08","09","10","11","12"),"2013"),paste(sep="/",c(paste(sep="","0",1:9),paste(sep="","1",0:2)),"2014")),t=1:17)
 dates$t<-datesDictionary$t[match(dates$monthYear,datesDictionary$monthYear)]
@@ -66,8 +65,8 @@ ttt<-long2wide(data=tt2,nameid="Sample.ID",namet="t",coly=c("TTR","InvA","Cultur
 ## load Ct values data
 ##
 
-ctSingleAssay<-read.csv("GordonMelita_StatsSupportAngeziwe/data/TTR & InvA master file Ct for correlation.csv",na.strings=c("Undetermined"))
-ctTAC<-read.csv("GordonMelita_StatsSupportAngeziwe/data/TAC Results_4Marc TAC TTR TAC InvA Ct For Correlation.csv",na.strings=c("0","Undetermined"))
+ctSingleAssay<-read.csv("data/TTR & InvA master file Ct for correlation.csv",na.strings=c("Undetermined"))
+ctTAC<-read.csv("data/TAC Results_4Marc TAC TTR TAC InvA Ct For Correlation.csv",na.strings=c("0","Undetermined"))
 ctSingleAssay[is.na(ctSingleAssay)]<-40
 ctTAC[is.na(ctTAC)]<-40
 # check with Ange that all NA/0/Undetermined values can be considered to have Ct>40
@@ -180,12 +179,12 @@ npvMat<-npvMat/npvMatCount
 colnames(rmCensCorMat)<-gsub(pattern="_bin",replacement="",gsub(pattern="_ct",replacement=" (Ct)",gsub(pattern="\\.",replacement="-",colnames(rmCensCorMat))))
 rownames(rmCensCorMat)<-gsub(pattern="_bin",replacement="",gsub(pattern="_ct",replacement=" (Ct)",gsub(pattern="\\.",replacement="-",rownames(rmCensCorMat))))
 
-png("GordonMelita_StatsSupportAngeziwe/output/lmmFits_CorrelationBetweenTestsRepeatedMeasurements_Ct.png",width=10,height=10,units="in",res=600)
+png("output/lmmFits_CorrelationBetweenTestsRepeatedMeasurements_Ct.png",width=10,height=10,units="in",res=600)
 corrplot(rmCensCorMat,is.corr=F,type="lower",cl.lim=c(0,1),method="number",tl.pos="lt",cl.pos="r",title="          repeated measures rank correlation",mar=c(0,0,1,0),cex.main=1.5,tl.cex=1.5,cl.cex=1.5,number.cex=1.5)
 corrplot(rmCensCorMat,is.corr=F,type="upper",cl.lim=c(0,1),tl.pos="n",add=T,cl.pos="n")
 dev.off()
 
-png("GordonMelita_StatsSupportAngeziwe/output/lmmFits_PositiveDiagnosticConcordance.png",width=10,height=10,units="in",res=600)
+png("output/lmmFits_PositiveDiagnosticConcordance.png",width=10,height=10,units="in",res=600)
 corrplot(ppvMat,is.corr=F,type="full",cl.lim=c(0,1),method="circle",tl.pos="lt",cl.pos="r",title="          concordance of positive diagnosis",mar=c(0,0,1,0),cex.main=1.5,tl.cex=1.5,cl.cex=1.5)
 for(i in 1:nrow(ppvMat)){
   for(j in 1:ncol(ppvMat)){
@@ -194,7 +193,7 @@ for(i in 1:nrow(ppvMat)){
 }
 dev.off()
 
-png("GordonMelita_StatsSupportAngeziwe/output/lmmFits_NegativeDiagnosticConcordance.png",width=10,height=10,units="in",res=600)
+png("output/lmmFits_NegativeDiagnosticConcordance.png",width=10,height=10,units="in",res=600)
 corrplot(npvMat,is.corr=F,type="full",cl.lim=c(0,1),method="circle",tl.pos="lt",cl.pos="r",title="          concordance of negative diagnosis",mar=c(0,0,1,0),cex.main=1.5,tl.cex=1.5,cl.cex=1.5)
 for(i in 1:nrow(npvMat)){
   for(j in 1:ncol(npvMat)){
@@ -203,7 +202,7 @@ for(i in 1:nrow(npvMat)){
 }
 dev.off()
 
-png("GordonMelita_StatsSupportAngeziwe/output/lmmFits_CorrelationAndConcordance.png",width=30,height=10,units="in",res=600)
+png("output/lmmFits_CorrelationAndConcordance.png",width=30,height=10,units="in",res=600)
 par(mfrow=c(1,3))
 
 corrplot(rmCensCorMat,is.corr=F,type="lower",cl.lim=c(0,1),method="number",tl.pos="lt",cl.pos="r",title="          repeated measures rank correlation",mar=c(0,0,1,0),cex.main=2,tl.cex=2,cl.cex=2,number.cex=2)
@@ -223,50 +222,3 @@ for(i in 1:nrow(npvMat)){
   }
 }
 dev.off()
-
-
-
-
-#########
-## OLD ##
-#########
-
-##--------------------
-#-- look at agreements between diagnostic tests (binary results only)
-##
-# agreeMat<-matrix(nrow=5,ncol=5)
-# tests<-colnames(agreeMat)
-# colnames(agreeMat)<-gsub(pattern="\\.",replacement="-",colnames(tt2)[3:7])
-# rownames(agreeMat)<-gsub(pattern="\\.",replacement="-",colnames(tt2)[3:7])
-# corMat<-agreeMat
-# ppvMat<-agreeMat
-# diag(corMat)<-1
-# for(j in 1:length(colnames(agreeMat))){
-#   for(i in 1:length(rownames(agreeMat))){
-#     var1<-gsub(pattern="-",replacement="\\.",rownames(agreeMat)[i])
-#     var2<-gsub(pattern="-",replacement="\\.",colnames(agreeMat)[j])
-#     agreeMat[i,j]<-sum(tt2[,var1]==tt2[,var2])/nrow(tt2)
-#     ppvMat[i,j]<-sum(tt2[,var1]==1 & tt2[,var2]==1)/sum(tt2[,var1]==1)
-#     if(i!=j){corMat[i,j]<-rmcorr(participant=factor(Sample.ID),measure1=var1,measure2=var2,dataset=tt2)$r}
-#   }
-# }
-# 
-# pdf("GordonMelita_StatsSupportAngeziwe/lmmFits_overallAgreementBetweenTests.pdf",width=10,height=10)
-# corrplot(agreeMat,is.corr=F,type="lower",cl.lim=c(0.85,1),method="number",tl.pos="lt",cl.pos="r")
-# corrplot(agreeMat,is.corr=F,type="upper",cl.lim=c(0.85,1),tl.pos="n",add=T)
-# dev.off()
-# 
-# pdf("GordonMelita_StatsSupportAngeziwe/lmmFits_CorrelationBetweenTestsRepeatedMeasurements.pdf",width=10,height=10)
-# corrplot(corMat,is.corr=F,type="lower",cl.lim=c(0,1),method="number",tl.pos="lt",cl.pos="r")
-# corrplot(corMat,is.corr=F,type="upper",cl.lim=c(0,1),tl.pos="n",add=T)
-# dev.off()
-# 
-# png("GordonMelita_StatsSupportAngeziwe/lmmFits_overallAgreementBetweenTests.png",width=10,height=10,units = "in", res = 300)
-# corrplot(agreeMat,is.corr=F,type="lower",cl.lim=c(0.85,1),method="number",tl.pos="lt",cl.pos="r")
-# corrplot(agreeMat,is.corr=F,type="upper",cl.lim=c(0.85,1),tl.pos="n",add=T)
-# dev.off()
-# 
-# png("GordonMelita_StatsSupportAngeziwe/lmmFits_CorrelationBetweenTestsRepeatedMeasurements.png",width=10,height=10,units = "in", res = 300)
-# corrplot(corMat,is.corr=F,type="lower",cl.lim=c(0,1),method="number",tl.pos="lt",cl.pos="r")
-# corrplot(corMat,is.corr=F,type="upper",cl.lim=c(0,1),tl.pos="n",add=T)
-# dev.off()
