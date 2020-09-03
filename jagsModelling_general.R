69afc16bc584483fb0bfe0eb178b8a856cf726b5
Marc Henrion <henrion.marc@gmail.com>
2020-09-03 13:54:08 +0200

    Adding data processing and model visualisation scripts.

---
+++ jagsModelling_general.R
@@ -0,0 +1,262 @@
+library(rjags)
+library(coda)
+
+rm(list=ls())
+#setwd("~/../work/MLW_LSTM/GordonMelita_StatsSupportAngeziwe")
+#setwd("~/work/GordonMelita_StatsSupportAngeziwe")
+
+outDir<-paste(sep="/","output",Sys.Date())
+if(!dir.exists(outDir)){dir.create(outDir)}
+
+##--------------------
+## set seed
+##
+
+todayDate<-as.integer(unlist(strsplit(split="-",as.character(Sys.Date()))))
+yy<-todayDate[1]
+mm<-todayDate[2]
+dd<-todayDate[3]
+randSeed<-rpois(1,lambda=sample(size=1,x=c(10,50,100,500,1000)))*dd+10*mm+yy
+
+set.seed(randSeed) # we will reset the seed before every call to jags.model()
+
+
+##--------------------
+## read data
+##
+
+tt<-read.csv("data/TTRInvASensitivity20170724_corrected.csv")
+tt<-tt[!is.na(tt$Sample.ID) & !is.na(tt$Visit..),]
+
+
+##--------------------
+## read recruitment dates and compute actual timepoints (in months)
+##
+
+dates<-read.csv("data/salexpoLIMSDataSetComplete.csv")
+dates$monthYear<-gsub("^[0-9]+/","",dates$DATE) # dates run from 08/2013 to 12/2014
+datesDictionary<-data.frame(monthYear=c(paste(sep="/",c("08","09","10","11","12"),"2013"),paste(sep="/",c(paste(sep="","0",1:9),paste(sep="","1",0:2)),"2014")),t=1:17)
+dates$t<-datesDictionary$t[match(dates$monthYear,datesDictionary$monthYear)]
+
+##--------------------
+## manual correction for now of the visit number for one sample - DOUBLE CHECK WITH ANGE AS THIS IS NOT 100% OBVIOUS - cf email to Ange from 17/04/2018
+tt$Visit..[tt$Sample.ID==59 & tt$Visit..==9 & tt$TTR=="neg"]<-8
+## end manual correction
+
+tt$t<-dates$t[match(paste(sep="::",tt$Sample.ID,tt$Visit..),paste(sep="::",dates$ID,gsub(pattern="Visit ",replacement="",dates$Visit)))]
+tt$monthYear<-datesDictionary$monthYear[match(tt$t,datesDictionary$t)]
+
+
+##--------------------
+## manual imputation (it's always very obvious) of "missing" dates for visits
+##
+
+# despite being so obvious: confirm with Ange
+tt$t[tt$Sample.ID==8 & tt$Visit..==11]<-12
+tt$t[tt$Sample.ID==10 & tt$Visit..==10]<-11
+tt$t[tt$Sample.ID==14 & tt$Visit..==7]<-8
+tt$t[tt$Sample.ID==14 & tt$Visit..==10]<-12
+tt$t[tt$Sample.ID==15 & tt$Visit..==7]<-8
+tt$t[tt$Sample.ID==17 & tt$Visit..==12]<-13
+tt$t[tt$Sample.ID==28 & tt$Visit..==5]<-7
+tt$t[tt$Sample.ID==51 & tt$Visit..==8]<-12
+tt$t[tt$Sample.ID==51 & tt$Visit..==11]<-15
+tt$t[tt$Sample.ID==53 & tt$Visit..==10]<-14
+
+
+##--------------------
+## recode t so that time proceeds monthly from the first visit - this is to avoid duplicated t values for a given individuals (problem caused by one visit early one month and another late the same month)
+##
+tt$t2<-tt$t
+tt$t2[tt$Sample.ID==3 & tt$Visit..==9]<-9
+tt$t2[tt$Sample.ID==3 & tt$Visit..==10]<-10
+tt$t2[tt$Sample.ID==11 & tt$Visit..==12]<-13
+tt$t2[tt$Sample.ID==21 & tt$Visit..==7]<-8
+tt$t2[tt$Sample.ID==24 & tt$Visit..==11]<-13
+tt$t2[tt$Sample.ID==40 & tt$Visit..==9]<-12
+tt$t2[tt$Sample.ID==40 & tt$Visit..==10]<-13    
+tt$t2[tt$Sample.ID==40 & tt$Visit..==11]<-14
+tt$t2[tt$Sample.ID==41 & tt$Visit..==7]<-10
+tt$t2[tt$Sample.ID==49 & tt$Visit..==6]<-10
+tt$t2[tt$Sample.ID==49 & tt$Visit..==7]<-11
+tt$t2[tt$Sample.ID==49 & tt$Visit..==9]<-13
+    
+##--------------------
+## recode test results in a better, cleaner way
+##
+
+tt2<-tt
+tt2$TTR<-ifelse(tolower(tt2$TTR)=="pos",1,ifelse(tolower(tt2$TTR)=="neg" | tolower(tt2$TTR)=="ind",0,NA))
+tt2$InvA<-ifelse(tolower(tt2$InvA)=="pos",1,ifelse(tolower(tt2$InvA)=="neg" | tolower(tt2$InvA)=="ind",0,NA))
+tt2$Culture<-ifelse(tolower(tt2$Culture)=="pos",1,ifelse(tolower(tt2$Culture)=="neg",0,NA))
+tt2$TAC.TTR<-ifelse(tolower(tt2$TAC.TTR)=="pos",1,ifelse(tolower(tt2$TAC.TTR)=="neg" | tolower(tt2$TAC.TTR)=="ind",0,NA))
+tt2$TAC.InvA<-ifelse(tolower(tt2$TAC.InvA)=="pos",1,ifelse(tolower(tt2$TAC.InvA)=="neg" | tolower(tt2$TAC.InvA)=="ind",0,NA))
+
+
+##--------------------
+## reformat for the way the JAGS/BUGS model file is set-up
+##
+
+uniqTimes<-sort(unique(tt2$t2))
+nT<-length(uniqTimes)
+uniqIds<-sort(unique(tt2$Sample.ID))
+nN<-length(uniqIds)
+uniqTests<-c("Culture","TTR","InvA","TAC.TTR","TAC.InvA")
+nK<-length(uniqTests)
+  
+
+dat<-list()
+dat$tests<-array(dim=c(nT,nN,nK))
+dimnames(dat$tests)<-list(paste(sep="","t",uniqTimes),paste(sep="","ind",uniqIds),uniqTests)
+for(k in 1:nK){
+  dat$tests[,,uniqTests[k]]<-matrix(nrow=nT,ncol=nN)
+}
+
+for(i_t in 1:nT){
+  for(i_n in 1:nN){
+    t<-uniqTimes[i_t]
+    id<-uniqIds[i_n]
+    for(i_k in 1:nK){
+      k<-uniqTests[i_k]
+      dat$tests[i_t,i_n,i_k]<-ifelse(sum(tt2$Sample.ID==id & tt2$t2==t)==1,tt2[[k]][tt2$Sample.ID==id & tt2$t2==t],NA)
+    }
+  }
+}
+
+dat$N<-nN
+dat$T<-nT
+dat$K<-nK
+
+
+##--------------------
+## run the models
+##
+
+set.seed(randSeed)
+jagsTimeHomoModel <- jags.model('scripts/jagsModelFile_TimeHomo_general.jags', data=dat, n.chains = 4, n.adapt = 1000)
+#jagsTimeHomoModel <- jags.model('GordonMelita_StatsSupportAngeziwe/jagsModelFile_TimeHomo.jags', data=dat, n.chains = 4, n.adapt = 0, inits=list(pInfInit=0.05, pUninf2Inf=0.05, pInf2Inf=0.08, crpCult=c(1-0,0.5),crpTtrStd=c(1-0.9,0.95),crpInvaStd=c(1-0.85,0.9),crpTtrTac=c(1-0.9,0.95),crpInvaTac=c(1-0.85,0.9)))
+#update(jagsTimeHomoModel,n.iter=50000) # note technicaly not required as we use coda.samples() below which also updates the model, but we use this as burn-in - so in total we will get 1,000 (adaptation iterations) + 10,000 (burn-in) + 100,000 (sampling) iterations
+parsTimeHomoModel<-coda.samples(model=jagsTimeHomoModel,variable.names=c('pInfInit','pUninf2Inf','pInf2Inf','crp'),n.iter=10000) # 5*2+2 = 12 parameters (each of the crp parameters is a vector of length 2); pUninf2Inf fixed to pInfInit
+save(list=ls(),file=paste(sep="/",outDir,"jags_AngeData_general_TimeHomoModel_TimeHomoModel.RData"))
+
+set.seed(randSeed)
+jagsTimeHeteroModel <- jags.model('scripts/jagsModelFile_TimeHetero_general.jags', data=dat, n.chains = 4, n.adapt = 1000)
+#jagsTimeHeteroModel <- jags.model('GordonMelita_StatsSupportAngeziwe/jagsModelFile_TimeHetero.jags', data=dat, n.chains = 4, n.adapt = 0, inits=list(pInfInit=0.05, pUninf2Inf=0.05, pInf2Inf=0.08, crpCult=c(1-0,0.5),crpTtrStd=c(1-0.9,0.95),crpInvaStd=c(1-0.85,0.9),crpTtrTac=c(1-0.9,0.95),crpInvaTac=c(1-0.85,0.9)))
+#update(jagsTimeHeteroModel,n.iter=50000) # note technicaly not required as we use coda.samples() below which also updates the model, but we use this as burn-in - so in total we will get 1,000 (adaptation iterations) + 10,000 (burn-in) + 100,000 (sampling) iterations
+parsTimeHeteroModel<-coda.samples(model=jagsTimeHeteroModel,variable.names=c('pInfInit','pUninf2Inf','pInf2Inf','crp'),n.iter=10000) # 5*2+(13-1)*2+1 = 35 parameters (each of the crp parameters is a vector of length 2); pUninf2Inf fixed to pInfInit
+save(list=ls(),file=paste(sep="/",outDir,"jags_AngeData_general_TimeHeteroModel_TimeHeteroModel.RData"))
+
+set.seed(randSeed)
+jagsTimeHomoMixedModel <- jags.model('scripts/jagsModelFile_TimeHomoMixed_general.jagss', data=dat, n.chains = 4, n.adapt = 1000)
+#jagsTimeHeteroMixedModel <- jags.model('GordonMelita_StatsSupportAngeziwe/jagsModelFile_TimeHeteroMixed.jags', data=dat, n.chains = 4, n.adapt = 1000)
+update(jagsTimeHomoMixedModel,n.iter=10000)
+parsTimeHomoMixedModel<-coda.samples(model=jagsTimeHomoMixedModel,variable.names=c('pInfInit','pUninf2Inf','pInf2Inf','crpNoRand','interceptStateCrp','slopeRandCrp'),n.iter=20000)
+save(list=setdiff(ls(),c("jagsTimeHomoModel","parsTimeHomoModel")),file=paste(sep="/",outDir,"jags_AngeData_general_TimeHomoMixedModel.RData"))
+
+#jagsTimeHeteroModel <- jags.model('GordonMelita_StatsSupportAngeziwe/jagsModelFile_TimeHetero.jags', data=dat, n.chains = 4, n.adapt = 1000)
+
+
+##--------------------
+## model comparison: DIC & PED
+##
+
+## A) standard DIC
+
+dicTimeHomoModel<-dic.samples(jagsTimeHomoModel,n.iter=5000,type="pD")
+dicTimeHeteroModel<-dic.samples(jagsTimeHeteroModel,n.iter=5000,type="pD")
+dicTimeHomoMixedModel<-dic.samples(jagsTimeHomoMixedModel,n.iter=5000,type="pD") # the penalty term fails to evaluate for some samples, hence need to deal with this; we do this in 2 ways, see below
+
+diffdic(dicTimeHomoModel,dicTimeHeteroModel)
+
+# way 1: impute NaNs in penalty terms to average penalty
+dicTimeHomoMixedModelNoMiss<-dicTimeHomoMixedModel
+dicTimeHomoMixedModelNoMiss$penalty[is.nan(dicTimeHomoMixedModelNoMiss$penalty)]<-mean(dicTimeHomoMixedModelNoMiss$penalty[!is.nan(dicTimeHomoMixedModelNoMiss$penalty)])
+
+diffdic(dicTimeHomoMixedModelNoMiss,dicTimeHomoModel)
+
+# way 2: compare only non-NaN samples
+idxNaN<-which(is.nan(dicTimeHomoMixedModel$penalty))
+dd<-dicTimeHomoModel
+ddMix<-dicTimeHomoMixedModel
+if(length(idxNaN)>0){
+  dd$penalty<-dd$penalty[-idxNaN]
+  dd$deviance<-dd$deviance[-idxNaN]
+  ddMix$penalty<-ddMix$penalty[-idxNaN]
+  ddMix$deviance<-ddMix$deviance[-idxNaN]
+}
+
+diffdic(ddMix,dd) # prefer the mixed model; just about
+
+
+## B) Penalized Expected Deviance (PED)
+
+pedTimeHomoModel<-dic.samples(jagsTimeHomoModel,n.iter=5000,type="popt")
+pedTimeHeteroModel<-dic.samples(jagsTimeHeteroModel,n.iter=5000,type="popt")
+pedTimeHomoMixedModel<-dic.samples(jagsTimeHomoMixedModel,n.iter=5000,type="popt") # the penalty term fails to evaluate for some samples, hence need to deal with this; we do this in 2 ways, see below
+
+# way 1: impute NaNs in penalty terms to average penalty
+pedTimeHomoMixedModelNoMiss<-pedTimeHomoMixedModel
+pedTimeHomoMixedModelNoMiss$penalty[is.nan(pedTimeHomoMixedModelNoMiss$penalty)]<-mean(pedTimeHomoMixedModelNoMiss$penalty[!is.nan(pedTimeHomoMixedModelNoMiss$penalty)])
+
+diffdic(pedTimeHomoMixedModelNoMiss,pedTimeHomoModel) # prefer the mixed model; somewhat
+
+# way 2: compare only non-NaN samples
+idxNaN<-which(is.nan(pedTimeHomoMixedModel$penalty))
+dd<-pedTimeHomoModel
+ddMix<-pedTimeHomoMixedModel
+if(length(idxNaN)>0){
+  dd$penalty<-dd$penalty[-idxNaN]
+  dd$deviance<-dd$deviance[-idxNaN]
+  ddMix$penalty<-ddMix$penalty[-idxNaN]
+  ddMix$deviance<-ddMix$deviance[-idxNaN]
+}
+
+diffdic(ddMix,dd) # prefer the standard model, somewhat
+
+
+
+##--------------------
+## MCMC diagnostics
+##
+
+print(summary(parsTimeHomoModel))
+print(gelman.diag(parsTimeHomoModel))
+
+pdf(paste(sep="/",outDir,"jags_AngeData_general_TimeHomoModel_MCMCdiagnostics.pdf"))
+plot(parsTimeHomoModel)
+dev.off()
+
+print(summary(parsTimeHeteroModel))
+print(gelman.diag(parsTimeHeteroModel))
+
+pdf(paste(sep="/",outDir,"jags_AngeData_general_TimeHeteroModel_MCMCdiagnostics.pdf"))
+plot(parsTimeHeteroModel)
+dev.off()
+
+print(gelman.diag(parsTimeHomoMixedModel))
+print(summary(parsTimeHomoMixedModel))
+
+pdf(paste(sep="/",outDir,"jags_AngeData_general_TimeHomoMixedModel_MCMCdiagnostics_new.pdf"))
+plot(parsTimeHomoMixedModel)
+dev.off()
+
+
+##--------------------
+## Bayesian model comparison: compute model evicences using nested sampling to compute Bayes factors
+##
+
+
+##--------------------
+## posterior predictive checks
+##
+
+
+##--------------------
+## save everything in one RData object
+##
+
+save(list=ls(),file=paste(sep="/",outDir,"jags_AngeData_general_everything.RData"))
+#setwd("~/../work/MLW_LSTM")
+setwd("~/work")
+
+
---
+++ summaryRealDatasetLMM_20200415.R
@@ -0,0 +1,281 @@
+library(MASS)
+library(RColorBrewer)
+library(HDInterval)
+library(rjags)
+
+rm(list=ls())
+
+# input arguments
+
+# args<-commandArgs(TRUE)
+# rdataJagsFile<-args[1] # typically "output/ToBeUsedForPaper/Real/jags_AngeData_general_TimeHomoModel.RData"
+# objName<-args[2] # typically "parsTimeHomoModel"
+# if(!is.na(args[3])){plotLims<-as.numeric(unlist(strsplit(split=",",args[3])))}else{plotLims<-NA}
+# if(!is.na(args[4])){testNames<-unlist(strsplit(split=",",args[4]))}else{testNames<-NA}; testNames[testNames=="NA"]<-NA
+# if(!is.na(args[5])){parsNames<-unlist(strsplit(split=",",args[5]))}else{parsNames<-NA}; parsNames[parsNames=="NA"]<-NA
+# if(!is.na(args[6])){trueSens<-as.numeric(unlist(strsplit(split=",",args[6])))}else{trueSens<-NA}
+# if(!is.na(args[7])){trueSpec<-as.numeric(unlist(strsplit(split=",",args[7])))}else{trueSpec<-NA}
+# if(!is.na(args[8])){outPrefixAdd<-args[8]}else{outPrefixAdd<-""}
+
+wd<-"/Users/marc/work/MLW_LSTM/GordonMelita_StatsSupportAngeziwe/"
+setwd(wd)
+rdataJagsFile<-"output/ToBeUsedForPaper/Real/jags_AngeData_general_TimeHomoModel.RData"
+objName<-"parsTimeHomoModel"
+plotLims<-NA
+testNames<-c("Culture","TTR","InvA","TAC-TTR","TAC-InvA")
+parsNames<-NA
+trueSens<-NA
+trueSpec<-NA
+outPrefixAdd<-"_manuscriptComments"
+
+outPrefix<-paste(sep="",gsub(rdataJagsFile,pattern=".RData",replacement=paste(sep="","_",gsub(Sys.Date(),pattern="-",replacement=""))),outPrefixAdd)
+
+
+# load input
+load(rdataJagsFile)
+mcmcObj<-get(objName)
+
+# define needed subroutines
+addWhisker<-function(low,high,anchor,whiskWidth=0.005,vertical=T,col="black"){
+      # low = lower whisker height; can be a vector
+      # high = upper whisker height; can be a vector
+      # anchor = position on which to anchor the whisker [x position if vertical==T, else y position]
+      # whiskWidth = width of whisker bars
+      # vertical = logical; if TRUE whiskers are plotted on the y axis, otherwise on the x axis
+
+    if(vertical){
+        segments(x0=anchor,x1=anchor,y0=low,y1=high,col=col)
+        segments(x0=anchor-whiskWidth,x1=anchor+whiskWidth,y0=low,y1=low,col=col)
+        segments(x0=anchor-whiskWidth,x1=anchor+whiskWidth,y0=high,y1=high,col=col)
+    }else{
+        segments(y0=anchor,y1=anchor,x0=low,x1=high,col=col)
+        segments(y0=anchor-whiskWidth,y1=anchor+whiskWidth,x0=low,x1=low,col=col)
+        segments(y0=anchor-whiskWidth,y1=anchor+whiskWidth,x0=high,x1=high,col=col)
+    }
+}
+
+darkenCol<-function(color,factor=1.5){
+    col<-col2rgb(color)
+    col<-col/factor
+    col<-rgb(t(col),maxColorValue=255)
+    col
+}
+
+addGrid<-function(doX=TRUE,doY=TRUE,xExt=c(0,1),xBwMinor=0.05,xBwMajor=0.1,yExt=xExt,yBwMinor=xBwMinor,yBwMajor=xBwMajor){
+    if(doX){
+        for(xTmp in seq(xExt[1],xExt[2],by=xBwMinor)){abline(v=xTmp,col="lightgray",lty=2,lwd=0.1)}
+        for(xTmp in seq(xExt[1],xExt[2],by=xBwMajor)){abline(v=xTmp,col="gray",lty=2,lwd=0.3)}
+    }
+    if(doY){
+        for(yTmp in seq(yExt[1],yExt[2],by=yBwMinor)){abline(h=yTmp,col="lightgray",lty=2,lwd=0.1)}
+        for(yTmp in seq(yExt[1],yExt[2],by=yBwMajor)){abline(h=yTmp,col="gray",lty=2,lwd=0.3)}
+    }
+}
+
+addAlpha <- function(COLORS, ALPHA){ # courtesy of https://menugget.blogspot.com/2012/04/adding-transparent-image-layer-to-plot.html#more
+     if(missing(ALPHA)) stop("provide a value for alpha between 0 and 1")
+      RGB <- col2rgb(COLORS, alpha=TRUE)
+      RGB[4,] <- round(RGB[4,]*ALPHA)
+      NEW.COLORS <- rgb(RGB[1,], RGB[2,], RGB[3,], RGB[4,], maxColorValue = 255)
+      return(NEW.COLORS)
+}
+
+getMaP<-function(dens,xPosterior,yPosterior,is.dens=T){
+    if(!is.dens){
+        dens<-kde2d(xPosterior,yPosterior,n=200)
+    }
+    idxMax<-which(dens$z==max(dens$z))
+    idxMaxX<-idxMax %% length(dens$y)
+    if(idxMaxX==0){idxMaxX<-length(dens$y)}
+    idxMaxY<-floor(idxMax / length(dens$x)) + 1
+    return(c(dens$x[idxMaxX],dens$y[idxMaxY]))
+}
+
+
+# sensitivities vs specificities
+nChains<-length(mcmcObj)
+nTests<-sum(grepl(pattern="crp",colnames(mcmcObj[[1]])))/2
+if(is.na(testNames[1])){testNames<-paste(sep="","test",1:nTests)}
+testsSens<-vector("list",nTests); names(testsSens)<-testNames
+testsSpec<-vector("list",nTests); names(testsSpec)<-testNames
+for(j in 1:nChains){
+    colnames(mcmcObj[[j]])<-gsub(pattern="NoRand",replacement="",colnames(mcmcObj[[j]]))
+    for(k in 1:nTests){
+        testsSens[[k]]<-c(testsSens[[k]],mcmcObj[[j]][,paste(sep="","crp[",k,",2]")])
+        testsSpec[[k]]<-c(testsSpec[[k]],1-mcmcObj[[j]][,paste(sep="","crp[",k,",1]")])
+    }
+}
+
+testsDens<-vector("list",nTests); names(testsDens)<-testNames
+for(k in 1:nTests){
+    testsDens[[k]]<-kde2d(testsSens[[k]],testsSpec[[k]],n=200)
+}
+
+cols<-colorRampPalette(brewer.pal(7,"Spectral")[-c(4,5)])(nTests)
+colsDarker<-cols
+for(i in 1:length(cols)){colsDarker[i]<-darkenCol(cols[i],factor=1.5)}
+
+if(is.na(plotLims[1])){plotLims<-c(0.35,1,0.8,1)}
+
+#png(paste(sep="",outPrefix,"_sensVSspec.png"),width=10,height=8,units="in",res=450)
+#png(paste(sep="",outPrefix,"_sensVSspec.png"),width=12,height=5,units="in",res=450)
+#pdf(paste(sep="",outPrefix,"_sensVSspec.pdf"),width=12,height=6)
+png(paste(sep="",outPrefix,"_sensVSspec.png"),width=12,height=6,units="in",res=600)
+#plot(xlim=plotLims[1:2],ylim=plotLims[3:4],main="",xlab="sensitivity",ylab="specificity",0:1,0:1,col=cols,pch=20,type="n")
+plot(xlim=plotLims[1:2],ylim=plotLims[3:4],main="",xlab="sensitivity",ylab="specificity",0:1,0:1,col=cols,pch=20,type="n",asp=1)
+addGrid()
+par(xpd=T)
+
+#colList<-data.frame(r=NA,g=NA,b=NA)
+#for(k in 1:nTests){
+#    colList[,c("r","g","b")]<-as.vector(col2rgb(colsDarker[k]))
+#    points(testsSens[[k]],testsSpec[[k]],pch=20,cex=0.2,col=rgb(red=colList$r,green=colList$g,blue=colList$b,alpha=10,maxColorValue=255))
+#}
+#for(k in 1:nTests){
+#    colTmp<-colorRampPalette(c("white",cols[k],colsDarker[k]))(200)
+#    colTmp<-addAlpha(colTmp,ALPHA=0.25)
+#    image(testsDens[[k]],col=colTmp,add=T)
+#}
+
+for(k in 1:nTests){
+  paletteCust<-function(n){
+    addAlpha(colorRampPalette(c("white",cols[k],cols[k],darkenCol(cols[k],factor=1.1)))(n),ALPHA=seq(0,1,length=n))
+  }
+  
+  .filled.contour(testsDens[[k]]$x,testsDens[[k]]$y,testsDens[[k]]$z,col=paletteCust(50),levels=seq(0,max(testsDens[[k]]$z),length=50))
+  contour(testsDens[[k]],col=addAlpha(colorRampPalette(c("white",cols[k]))(25),ALPHA=seq(0.2,0.8,length=25)),add=T,drawlabels=F,levels=seq(4,max(testsDens[[k]]$z),length=25))
+}
+
+fullMCMCtable<-mcmcObj[[1]]
+for(i in 2:length(mcmcObj)){
+  fullMCMCtable<-rbind(fullMCMCtable,mcmcObj[[i]])
+}
+fullMCMCtable<-as.data.frame(fullMCMCtable)
+nc<-ncol(fullMCMCtable)
+
+fullMCMCtable$ppvCult<-fullMCMCtable$`crp[1,2]`*fullMCMCtable$pInfInit / (fullMCMCtable$`crp[1,2]`*fullMCMCtable$pInfInit + fullMCMCtable$`crp[1,1]`*(1-fullMCMCtable$pInfInit))
+fullMCMCtable$ppvTTR<-fullMCMCtable$`crp[2,2]`*fullMCMCtable$pInfInit / (fullMCMCtable$`crp[2,2]`*fullMCMCtable$pInfInit + fullMCMCtable$`crp[2,1]`*(1-fullMCMCtable$pInfInit))
+fullMCMCtable$ppvInvA<-fullMCMCtable$`crp[3,2]`*fullMCMCtable$pInfInit / (fullMCMCtable$`crp[3,2]`*fullMCMCtable$pInfInit + fullMCMCtable$`crp[3,1]`*(1-fullMCMCtable$pInfInit))
+fullMCMCtable$ppvTacTTR<-fullMCMCtable$`crp[4,2]`*fullMCMCtable$pInfInit / (fullMCMCtable$`crp[4,2]`*fullMCMCtable$pInfInit + fullMCMCtable$`crp[4,1]`*(1-fullMCMCtable$pInfInit))
+fullMCMCtable$ppvTacInvA<-fullMCMCtable$`crp[5,2]`*fullMCMCtable$pInfInit / (fullMCMCtable$`crp[5,2]`*fullMCMCtable$pInfInit + fullMCMCtable$`crp[5,1]`*(1-fullMCMCtable$pInfInit))
+
+fullMCMCtable$npvCult<-(1-fullMCMCtable$`crp[1,1]`)*(1-fullMCMCtable$pInfInit) / ((1-fullMCMCtable$`crp[1,2]`)*(1-fullMCMCtable$pInfInit) + (1-fullMCMCtable$`crp[1,1]`)*(1-fullMCMCtable$pInfInit))
+fullMCMCtable$npvTTR<-(1-fullMCMCtable$`crp[2,1]`)*(1-fullMCMCtable$pInfInit) / ((1-fullMCMCtable$`crp[2,2]`)*(1-fullMCMCtable$pInfInit) + (1-fullMCMCtable$`crp[2,1]`)*(1-fullMCMCtable$pInfInit))
+fullMCMCtable$npvInvA<-(1-fullMCMCtable$`crp[3,1]`)*(1-fullMCMCtable$pInfInit) / ((1-fullMCMCtable$`crp[3,2]`)*(1-fullMCMCtable$pInfInit) + (1-fullMCMCtable$`crp[3,1]`)*(1-fullMCMCtable$pInfInit))
+fullMCMCtable$npvTacTTR<-(1-fullMCMCtable$`crp[4,1]`)*(1-fullMCMCtable$pInfInit) / ((1-fullMCMCtable$`crp[4,2]`)*(1-fullMCMCtable$pInfInit) + (1-fullMCMCtable$`crp[4,1]`)*(1-fullMCMCtable$pInfInit))
+fullMCMCtable$npvTacInvA<-(1-fullMCMCtable$`crp[5,1]`)*(1-fullMCMCtable$pInfInit) / ((1-fullMCMCtable$`crp[5,2]`)*(1-fullMCMCtable$pInfInit) + (1-fullMCMCtable$`crp[5,1]`)*(1-fullMCMCtable$pInfInit))
+
+sensEstimates<-data.frame(matrix(nrow=nTests,ncol=6))
+rownames(sensEstimates)<-testNames
+colnames(sensEstimates)<-c("lower95SymCI","median","upper95SymCI","lower95HdiCI","MaP","upper95HdiCI")
+specEstimates<-sensEstimates
+ppvEstimates<-sensEstimates
+npvEstimates<-specEstimates
+hdiTmp<-hdi(mcmcObj,credMass=0.95)
+hdiTmp2<-hdi(fullMCMCtable[,(nc+1):(nc+2*nTests)],credMass=0.95)
+for(k in 1:nTests){
+    sensEstimates[k,1:3]<-summary(mcmcObj)$quantiles[paste(sep="","crp[",k,",2]"),c("2.5%","50%","97.5%")]
+    specEstimates[k,1:3]<-1-summary(mcmcObj)$quantiles[paste(sep="","crp[",k,",1]"),c("97.5%","50%","2.5%")]
+    map<-getMaP(dens=testsDens[[k]])
+    sensEstimates$MaP[k]<-map[1]
+    specEstimates$MaP[k]<-map[2]
+    sensEstimates$lower95HdiCI[k]<-hdiTmp["lower",paste(sep="","crp[",k,",2]")]
+    sensEstimates$upper95HdiCI[k]<-hdiTmp["upper",paste(sep="","crp[",k,",2]")]
+    specEstimates$lower95HdiCI[k]<-1-hdiTmp["upper",paste(sep="","crp[",k,",1]")]
+    specEstimates$upper95HdiCI[k]<-1-hdiTmp["lower",paste(sep="","crp[",k,",1]")]
+    
+    ppvEstimates[k,1:3]<-quantile(probs=c(0.025,0.5,0.975),fullMCMCtable[,nc+k])
+    npvEstimates[k,1:3]<-quantile(probs=c(0.025,0.5,0.975),fullMCMCtable[,nc+nTests+k])
+    map<-getMaP(is.dens=F,xPosterior=fullMCMCtable[,nc+k],yPosterior=fullMCMCtable[,nc+nTests+k])
+    ppvEstimates$MaP[k]<-map[1]
+    npvEstimates$MaP[k]<-map[2]
+    ppvEstimates$lower95HdiCI[k]<-hdiTmp2["lower",k]
+    ppvEstimates$upper95HdiCI[k]<-hdiTmp2["upper",k]
+    npvEstimates$lower95HdiCI[k]<-hdiTmp2["lower",nTests+k]
+    npvEstimates$upper95HdiCI[k]<-hdiTmp2["upper",nTests+k]
+}
+
+addWhisker(low=sensEstimates$lower95HdiCI,high=sensEstimates$upper95HdiCI,anchor=specEstimates$MaP,vertical=F,col=cols,whiskWidth=0.005/((1-0.35)/(1-0.85)))
+addWhisker(low=specEstimates$lower95HdiCI,high=specEstimates$upper95HdiCI,anchor=sensEstimates$MaP,vertical=T,col=cols)
+
+points(sensEstimates$MaP,specEstimates$MaP,col=cols,pch=19,cex=1.5)
+
+if(!is.na(trueSens[1]) & !is.na(trueSpec[1])){points(trueSens,trueSpec,col=cols,pch=4,cex=1)}
+
+legend(x="bottomleft",pch=c(rep(19,5)),col=cols,bty="n",legend=testNames)
+dev.off()
+
+
+# pUninf2Inf, pInf2Inf
+pUninf2InfMCMC<-numeric(0)
+pInf2InfMCMC<-numeric(0)
+for(j in 1:nChains){
+    pUninf2InfMCMC<-c(pUninf2InfMCMC,mcmcObj[[j]][,"pUninf2Inf"])
+    pInf2InfMCMC<-c(pInf2InfMCMC,mcmcObj[[j]][,"pInf2Inf"])
+}
+
+#pUninf2InfDens<-density(pUninf2InfMCMC)
+#pInf2InfDens<-density(pInf2InfMCMC)
+mapEstimatesTransitionMatrix<-getMaP(is.dens=F,xPosterior=pUninf2InfMCMC,yPosterior=pInf2InfMCMC)
+
+# caterpillar plots, posterior density estimates, prior distributions
+#nPars<-ncol(mcmcObj[[1]])
+#if(is.na(parsNames[1])){parsNames<-colnames(mcmcObj[[1]])}
+
+
+# summary table of model parameters for paper: sensitivity, specificity, PPV, NPV (posterior median, MAP, 95% HDI CrI)
+estTable<-matrix(nrow=nTests,ncol=4*3)
+rownames(estTable)<-testNames
+colnames(estTable)<-apply(FUN=paste,MARGIN=1,collapse="_",expand.grid(c("postMedian","MAP","HDI"),c("sens","spec","ppv","npv"))[,2:1])
+estTable<-as.data.frame(estTable)
+for(k in 1:nTests){
+  estTable$sens_postMedian[k]<-format(nsmall=4,round(digits=4,sensEstimates$median[k]))
+  estTable$sens_MAP[k]<-format(nsmall=4,round(digits=4,sensEstimates$MaP[k]))
+  estTable$sens_HDI[k]<-paste(sep="","(",paste(collapse=",",format(nsmall=4,round(digits=4,as.matrix(sensEstimates[k,c("lower95HdiCI","upper95HdiCI")])))),")")
+  
+  estTable$spec_postMedian[k]<-format(nsmall=4,round(digits=4,specEstimates$median[k]))
+  estTable$spec_MAP[k]<-format(nsmall=4,round(digits=4,specEstimates$MaP[k]))
+  estTable$spec_HDI[k]<-paste(sep="","(",paste(collapse=",",format(nsmall=4,round(digits=4,as.matrix(specEstimates[k,c("lower95HdiCI","upper95HdiCI")])))),")")
+  
+  estTable$ppv_postMedian[k]<-format(nsmall=4,round(digits=4,ppvEstimates$median[k]))
+  estTable$ppv_MAP[k]<-format(nsmall=4,round(digits=4,ppvEstimates$MaP[k]))
+  estTable$ppv_HDI[k]<-paste(sep="","(",paste(collapse=",",format(nsmall=4,round(digits=4,as.matrix(ppvEstimates[k,c("lower95HdiCI","upper95HdiCI")])))),")")
+  
+  estTable$npv_postMedian[k]<-format(nsmall=4,round(digits=4,npvEstimates$median[k]))
+  estTable$npv_MAP[k]<-format(nsmall=4,round(digits=4,npvEstimates$MaP[k]))
+  estTable$npv_HDI[k]<-paste(sep="","(",paste(collapse=",",format(nsmall=4,round(digits=4,as.matrix(npvEstimates[k,c("lower95HdiCI","upper95HdiCI")])))),")")
+}
+
+write.table(estTable,sep="\t",row.names=F,col.names=T,quote=F,file=paste(sep="",outPrefix,"_sensSpecPpvNpv_Table.tab"))
+
+# PPV vs. NPV plot
+plotLims<-c(0.2,1,0.6,1)
+#pdf(paste(sep="",outPrefix,"_ppvVSnpv.pdf"),width=12,height=8)
+png(paste(sep="",outPrefix,"_ppvVSnpv.png"),width=12,height=8,units="in",res=600)
+plot(xlim=plotLims[1:2],ylim=plotLims[3:4],main="",xlab="positive predictive value",ylab="negative predictive value",0:1,0:1,col=cols,pch=20,type="n",asp=1)
+addGrid()
+par(xpd=T)
+
+testsDens<-vector("list",nTests); names(testsDens)<-testNames
+for(k in 1:nTests){
+  testsDens[[k]]<-kde2d(fullMCMCtable[,nc+k],fullMCMCtable[,nc+nTests+k],n=250)
+}
+for(k in 1:nTests){
+  paletteCust<-function(n){
+    addAlpha(colorRampPalette(c("white",cols[k],cols[k],darkenCol(cols[k],factor=1.1)))(n),ALPHA=seq(0,1,length=n))
+  }
+  
+  .filled.contour(testsDens[[k]]$x,testsDens[[k]]$y,testsDens[[k]]$z,col=paletteCust(50),levels=seq(0,max(testsDens[[k]]$z),length=50))
+  contour(testsDens[[k]],col=addAlpha(colorRampPalette(c("white",cols[k]))(25),ALPHA=seq(0.2,0.8,length=25)),add=T,drawlabels=F,levels=seq(8,max(testsDens[[k]]$z),length=25))
+}
+
+addWhisker(low=ppvEstimates$lower95HdiCI,high=ppvEstimates$upper95HdiCI,anchor=npvEstimates$MaP,vertical=F,col=cols,whiskWidth=0.005/((1-0.35)/(1-0.85)))
+addWhisker(low=npvEstimates$lower95HdiCI,high=npvEstimates$upper95HdiCI,anchor=ppvEstimates$MaP,vertical=T,col=cols)
+
+points(ppvEstimates$MaP,npvEstimates$MaP,col=cols,pch=19,cex=1.5)
+
+legend(x="bottomleft",pch=c(rep(19,5)),col=cols,bty="n",legend=testNames)
+dev.off()
+
+
+save(list=ls(),file=paste(sep="",outPrefix,"_summaryMetrics.RData"))
